9ab280eda6ab86a501afcd036f9435ba91642d63
Benjy Marks <benjy.marks@sydney.edu.au>
2020-07-26 22:56:42 +0000

    moved to pyproject.toml for requirements

--- plotting.py
+++ plotting.py
@@ -2,5 +2,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 def show_world(world):
+    plt.ion() # turn on interactive mode
     plt.imshow(world)
-    plt.show()
+    plt.colorbar()
+    plt.pause(0.1) # wait a bit so we can see the graph
+    # plt.show() # not needed in interactive mode
